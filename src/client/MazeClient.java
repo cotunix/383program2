b2c6314158336d6352c3f854990df90c731222b6
Kyle Richardson <richarkc@miamioh.edu>
2015-09-25 19:36:53 -0400

    Added XMLRPC helpers

--- src/client/MazeClient.java
+++ src/client/MazeClient.java
@@ -3,8 +3,23 @@ package client;
 import java.net.*;
 import java.util.*;
 import java.io.*;
-import org.apache.xmlrpc.*;
+
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 
 public class MazeClient {
+	XmlRpcClient xmlRPCclient = null;
+	int port = -1;
+	String id = "";
+	String username;
+	String password;
+	
+	public MazeClient(String host,int p) throws IOException {
+		port = p;
+		XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+		config.setServerURL(new URL("http://" + host+":"+port));
+		xmlRPCclient = new XmlRpcClient();
+		xmlRPCclient.setConfig(config);
+	}
 
 }
