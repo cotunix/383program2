63efbe3e6ad56da1c870a238d8531e5f8fe35f73
Kyle Richardson <richarkc@miamioh.edu>
2015-09-25 23:22:19 -0400

    Made some changes son

--- src/server/DB.java
+++ src/server/DB.java
@@ -8,8 +8,6 @@ public class DB {
 	private String user = "mazeuser";
 	private String pwd = "383";
 	private Statement state;
-	
-	
 
 	public DB() {
 		try {
@@ -29,36 +27,40 @@ public class DB {
 				+ 0 + ", " + "'ACTIVE')";
 		state.executeUpdate(query);
 	}
-	
+
 	public int[] getXY(String user) throws SQLException {
-		
-		String query = "SELECT x, y, state FROM maze WHERE user = " + "\"" + user + "\"";
+
+		String query = "SELECT x, y, state FROM maze WHERE user = " + "\""
+				+ user + "\"";
 		ResultSet rs = state.executeQuery(query);
 		rs.next();
 		if (!rs.getString("state").equals("ACTIVE")) {
 			throw new SQLException("User state not ACTIVE");
 		}
-		int[] xy = {rs.getInt("x"), rs.getInt("y")};
+		int[] xy = { rs.getInt("x"), rs.getInt("y") };
 		return xy;
 	}
-	
+
 	public void move(int x, int y, String user) throws SQLException {
-		String query = "UPDATE maze SET X = " + x + ", Y = " + y + ", moves = moves + 1 WHERE user = \"" + user + "\"";
+		String query = "UPDATE maze SET X = " + x + ", Y = " + y
+				+ ", moves = moves + 1 WHERE user = \"" + user + "\"";
 		state.executeUpdate(query);
-		
+
 	}
-	
+
 	public void deleteUser(String user) throws SQLException {
-		String sql = "DELETE FROM maze WHERE user = " + "\"" + user + "\"";
-		state.executeUpdate(sql);
+		String query = "DELETE FROM maze WHERE user = " + "\"" + user + "\"";
+		state.executeUpdate(query);
 	}
-	
+
 	public void finished(String user) throws SQLException {
-		String sql = "UPDATE maze SET state = 'FINISHED' WHERE user = " + "\"" + user + "\"";
-		state.executeUpdate(sql);
+		String query = "UPDATE maze SET state = 'FINISHED' WHERE user = " + "\""
+				+ user + "\"";
+		state.executeUpdate(query);
 	}
-	
-	public ResultSet getMaze()throws SQLException {
-		return state.executeQuery("SELECT * FROM maze");
+
+	public ResultSet getMaze() throws SQLException {
+		String query = "SELECT * FROM maze";
+		return state.executeQuery(query);
 	}
 }
